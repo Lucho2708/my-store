36bb806fb8bcfe3a4a86506be246a510c2bbfc03
Luis Eduardo Silva Rodríguez <lsilva@toyotacredito.com.co>
2023-04-04 17:21:03 -0500

    07. Property Binding es la manera que dispone Angular para controlar y modificar las propiedades de los distintos elementos de HTML. Para esto, simplemente utiliza los corchetes [] para poder modificar dinámicamente ese atributo desde el controlador.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Luis';
-  age = 18;
+  age = 30;
   image = 'https://i.blogs.es/944446/naruto-el-anime-original-regresa-con-cuatro-nuevos-episodios-por-el-20-aniversario-de-la-serie11/1024_2000.jpeg';
+  btnDisabled = true;
+  person = {
+    name: 'Luis Eduardo Silva',
+    age: 35,
+    image: 'https://i.blogs.es/944446/naruto-el-anime-original-regresa-con-cuatro-nuevos-episodios-por-el-20-aniversario-de-la-serie11/1024_2000.jpeg'
+  }
 }
