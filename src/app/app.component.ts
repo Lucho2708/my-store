98015cf26b965ddcf3fbe4943f82d5f79601d27b
Luis Eduardo Silva Rodríguez <lsilva@toyotacredito.com.co>
2023-04-04 17:05:33 -0500

    06. String interpolation
    Funciona para colocar logica, llamar variables y entre otras cosas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Luis';
+  age = 18;
+  image = 'https://i.blogs.es/944446/naruto-el-anime-original-regresa-con-cuatro-nuevos-episodios-por-el-20-aniversario-de-la-serie11/1024_2000.jpeg';
 }
